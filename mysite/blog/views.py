fd074c91f5d7694a746a5490b23295b32d51f37c
jaloliddinofficial <yjaloliddin.official@gmail.com>
2024-04-03 13:53:50 +0500

    new reales 3.0.0

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,11 +3,12 @@ from .models import Post, Comment
 from django.http import Http404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, TrigramSimilarity
 
 def post_share(request, post_pk):
     post = get_object_or_404(Post,
@@ -116,5 +117,29 @@ def post_comment(request, post_pk):
     return render(
         request=request,
         template_name='blog/post/comment.html',
+        context=context
+    )
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            search_vector = SearchVector('title', weight="A")+SearchVector('body', weight="B")
+            search_query = SearchQuery(query)
+            results = Post.published.annotate(similarity=TrigramSimilarity('title', query),
+                                              ).filter(similarity__gt=0.1).order_by('-similarity')
+    context = {
+        'form': form,
+        'query': query,
+        'results': results,
+    }
+    return render(
+        request=request,
+        template_name='blog/post/search.html',
         context=context
     )
